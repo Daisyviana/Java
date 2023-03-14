9e718e8781a901d5dbef1f59b40dd28449d218b2
Daisy Kelly Viana <daisyviana96@gmail.com>
2023-03-14 11:53:00 -0300

    Projeto Wello Word

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        int teste = 10;   
+		
+		System.out.println("Hello World!");
+		System.out.println("Olá Turma 62)");
+       System.out.println("Variavel teste: " + teste);
+	}
+
+}
